fa9a89f1d1e5fc224b60ac289d930e9dfce670b9
Hayri Ozdemir <hayri@MASTUDE5GU0Q6L4.cust.communityfibre.co.uk>
2024-07-31 23:51:36 +0100

    teestin codedeploy ,adding appspec and scripts files

---
+++ scripts/application_start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Navigate to app folder
+cd /home/ec2-user/cavalieri-acebook
+
+# Start the application with PM2
+echo "Starting application with PM2"
+pm2 start npm --name "cavalieri-acebook" -- start
---
+++ scripts/before_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Update package manager and install Node.js
+echo "Installing node.js"
+sudo yum update -y
+curl -sL https://rpm.nodesource.com/setup_22.x | sudo bash -
+sudo yum install -y nodejs
+
+# Install PM2 globally
+echo "Installing PM2"
+sudo npm install pm2@latest -g
