c76d0b8a83e7f7da0179b4ec4574886287b6259a
Hayri Ozdemir <hayri@MASTUDE5GU0Q6L4.cust.communityfibre.co.uk>
2024-08-01 13:41:00 +0100

    testing scripts files if they are correct

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,7 +5,3 @@ cd /home/ec2-user/cavalieri-acebook
 # Install dependencies
 echo "Installing dependencies"
 sudo -u ec2-user npm ci
-
-# Build the application if necessary
-echo "Building the application"
-sudo -u ec2-user npm run build
