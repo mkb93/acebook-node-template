cb1c4cc6fa482b3225693e680ee65cd04fddba40
Hayri Ozdemir <hayri@MASTUDE5GU0Q6L4.cust.communityfibre.co.uk>
2024-08-01 20:48:08 +0100

    test

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -27,7 +27,7 @@ const SessionsController = {
     if (req.session.user && req.cookies.user_sid) {
       res.clearCookie("user_sid");
     }
-    res.redirect("/");
+    res.redirect("/sessions/new");
   },
 };
 
