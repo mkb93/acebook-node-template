09ddfedcf928c9cb3edb5e6238cb238019c7dc86
Hayri Ozdemir <hayri@MASTUDE5GU0Q6L4.cust.communityfibre.co.uk>
2024-08-02 12:21:45 +0100

    Css file updated, new style added ,on controllers sessions the routes changed

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -12,9 +12,9 @@ const SessionsController = {
 
     User.findOne({ email: email }).then((user) => {
       if (!user) {
-        res.redirect("/sessions/new");
+        res.redirect("/");
       } else if (user.password != password) {
-        res.redirect("/sessions/new");
+        res.redirect("/");
       } else {
         req.session.user = user;
         res.redirect("/posts");
